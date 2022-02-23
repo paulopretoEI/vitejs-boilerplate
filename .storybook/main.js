d791872ab5da746b9c1328e0159a4b9e5376d584
Paulo Preto <pauloa_preto@hotmail.com>
2022-02-23 21:17:39 +0000

    add tailwind to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,18 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   "framework": "@storybook/react",
   "core": {
