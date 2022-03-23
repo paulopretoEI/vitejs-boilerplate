68d00f48ec175b45f2db6f31a134b6a7c86f3a61
Paulo Preto <pauloa_preto@hotmail.com>
2022-03-23 21:06:53 +0000

    remove conflict plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import react from '@vitejs/plugin-react';
-import reactRefresh from '@vitejs/plugin-react-refresh';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths()],
 });
